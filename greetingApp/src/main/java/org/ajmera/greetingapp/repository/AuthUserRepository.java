8739e5709e2162199094051a9f67c29eee54db3a
PankajAjmera1 <coderajmera@gmail.com>
2025-03-05 01:45:41 +0530

    Register & Login with Hashed Password

---
+++ greetingApp/src/main/java/org/ajmera/greetingapp/repository/AuthUserRepository.java
@@ -0,0 +1,12 @@
+package org.ajmera.greetingapp.repository;
+
+import org.ajmera.greetingapp.model.AuthUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthUserRepository extends JpaRepository<AuthUser,Long> {
+    <optional> AuthUser findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
